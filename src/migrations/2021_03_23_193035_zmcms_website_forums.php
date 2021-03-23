97c70fbb81d3a1611dcc6b837a81af754e3b8b94
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-23 22:30:31 +0100

    Update 2021_03_23_193035_zmcms_website_forums.php
    Lista forum

--- src/migrations/2021_03_23_193035_zmcms_website_forums.php
+++ src/migrations/2021_03_23_193035_zmcms_website_forums.php
@@ -6,6 +6,29 @@ use Illuminate\Support\Facades\DB;
 
 class ZmcmsWebsiteForums extends Migration{
 	public function up(){
+		$tblNamePrefix=(Config('database.prefix')??'');
+		$tblName=$tblNamePrefix.'website_forums';
+		Schema::create($tblName, function($table){$table->string('token', 70);});
+		Schema::table($tblName, function($table){$table->string('access', 70)->default('*');}); // Info, które grupy użytkowników mają dostęp do danego forum
+		Schema::table($tblName, function($table){$table->string('active', 1);}); //Aktywny - 1, Nieaktywny -0. Aktywny się wyświetla, nieaktywny nie.
+		Schema::table($tblName, function($table){$table->string('obj_type', 70);});
+		Schema::table($tblName, function($table){$table->string('obj_token', 70);});
+		Schema::table($tblName, function($table){$table->primary(['token',], 'zmcmswfwnkey0');});
+		Schema::table($tblName, function($table){$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));});//Imię
+		Schema::table($tblName, function($table){$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));});//Imię
+
+
+		$tblName=$tblNamePrefix.'website_forums_names'; // Nazwy forum
+		Schema::create($tblName, function($table){$table->string('token', 70);});
+		Schema::table($tblName, function($table){$table->string('langs_id', 5);}); // 
+		Schema::table($tblName, function($table){$table->string('name', 5);}); // 
+		Schema::table($tblName, function($table){$table->string('slug', 5)->unique();}); // 
+		Schema::table($tblName, function($table){$table->text('intro');}); // 
+		Schema::table($tblName, function($table){$table->primary(['token', 'langs_id'], 'zmcmswfwnkey1');});
+		Schema::table($tblName, function($table){$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));});//Imię
+		Schema::table($tblName, function($table){$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));});
+		Schema::table($tblName, function($table) use ($tblNamePrefix){$table->foreign('token')->references('token')->on($tblNamePrefix.'website_forums')->onUpdate('cascade')->onDelete('cascade');});
+
 	}
 	public function down(){
 	}
