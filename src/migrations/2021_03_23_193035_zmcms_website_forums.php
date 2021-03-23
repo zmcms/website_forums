1f18e2cb05efc874d169196163494dd23dc84fb9
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-23 22:57:38 +0100

    Update 2021_03_23_193035_zmcms_website_forums.php

--- src/migrations/2021_03_23_193035_zmcms_website_forums.php
+++ src/migrations/2021_03_23_193035_zmcms_website_forums.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 class ZmcmsWebsiteForums extends Migration{
 	public function up(){
 		$tblNamePrefix=(Config('database.prefix')??'');
+		
 		$tblName=$tblNamePrefix.'website_forums';
 		Schema::create($tblName, function($table){$table->string('token', 70);});
 		Schema::table($tblName, function($table){$table->string('access', 70)->default('*');}); // Info, które grupy użytkowników mają dostęp do danego forum
@@ -28,6 +29,24 @@ class ZmcmsWebsiteForums extends Migration{
 		Schema::table($tblName, function($table){$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));});//Imię
 		Schema::table($tblName, function($table){$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));});
 		Schema::table($tblName, function($table) use ($tblNamePrefix){$table->foreign('token')->references('token')->on($tblNamePrefix.'website_forums')->onUpdate('cascade')->onDelete('cascade');});
+
+		$tblName=$tblNamePrefix.'website_forums_comments';
+		Schema::create($tblName, function($table){$table->string('token', 70);}); // Identyfikator rekordu 
+		Schema::table($tblName, function($table){$table->string('token_reply', 70)->nullable();}); // Gdy odpowiadamy na jakiś komentarz, ustawiamy tutaj identyfikator jego rekordu
+		Schema::table($tblName, function($table){$table->string('forum_token', 70);});
+		Schema::table($tblName, function($table){$table->string('langs_id', 5);});
+		Schema::table($tblName, function($table){$table->string('active', 1);});
+		Schema::table($tblName, function($table){$table->string('activation_token', 70)->nullable();}); // Null, jeśli komentarz zweryfikowany, wtedy active = 1. Losowa wartość, gdy trzeba zweryfikować.
+		Schema::table($tblName, function($table){$table->string('email', 50);});
+		Schema::table($tblName, function($table){$table->string('show_email', 1);}); //Użytkownik może zdecydować, czy pokazać swój mail, czy nie
+		Schema::table($tblName, function($table){$table->string('nick', 50);});
+		Schema::table($tblName, function($table){$table->text('comment');});
+		Schema::table($tblName, function($table){$table->primary(['token',], 'zmcmswfwnkey2');});
+		Schema::table($tblName, function($table){$table->index('email', 50);});
+		Schema::table($tblName, function($table){$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));});//Imię
+		Schema::table($tblName, function($table){$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));});
+		Schema::table($tblName, function($table) use ($tblNamePrefix){$table->foreign('forum_token')->references('token')->on($tblNamePrefix.'website_forums')->onUpdate('cascade')->onDelete('cascade');});
+
 
 	}
 	public function down(){
