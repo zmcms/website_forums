a4e4d82dfed9d33b8387ef622c85748b907ae32a
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-24 09:09:03 +0100

    Update home_subview.blade.php

--- src/backend/views/home_subview.blade.php
+++ src/backend/views/home_subview.blade.php
@@ -1,5 +1,5 @@
 <div class="home_module home_module micro12 small6 medium2">
     <h1><span class="fas fa-bars"></span> Fora</h1>
-    <button id="btn_zwal">Lista</button>
-    <button id="btn_zwan">Nowe forum</button>
+    <button id="btn_zfhal">Lista</button>
+    <button id="btn_zfha2">Nowe forum</button>
 </div>
