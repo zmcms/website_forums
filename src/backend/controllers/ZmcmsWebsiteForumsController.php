0fef9a4b8e58abd7d0d1f91593f3fc33f54bc735
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-24 09:10:00 +0100

    Update ZmcmsWebsiteForumsController.php

--- src/backend/controllers/ZmcmsWebsiteForumsController.php
+++ src/backend/controllers/ZmcmsWebsiteForumsController.php
@@ -3,5 +3,12 @@ namespace Zmcms\WebsiteForums\Backend\Controllers;
 use Illuminate\Http\Request;
 class ZmcmsWebsiteForumsController extends \App\Http\Controllers\Controller
 {
-
+	public function forums_list(){}
+	public function forums_save(){}
+	public function zmcms_website_forums_filter(){}
+	public function forums_edit(){}
+	public function zmcms_website_forums_paginator(){}
+	public function zmcms_website_forums_page_delete(){}
+	public function forums_create_frm(){}
+	public function zmcms_website_article_delete(){}
 }
