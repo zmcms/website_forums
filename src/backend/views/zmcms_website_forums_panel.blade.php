8702591d893289604c522178131af36349d7cc2e
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-29 13:06:22 +0200

    Update zmcms_website_forums_panel.blade.php

--- src/backend/views/zmcms_website_forums_panel.blade.php
+++ src/backend/views/zmcms_website_forums_panel.blade.php
@@ -7,12 +7,24 @@
 	</div>
 </div>
 <div id="zmcms_website_articles_control_panel" class="control_subbelt_belt">
-	<button id="btn_article_new"><span class="fas fa-plus"></span> {{___('Nowy artykuł')}}</button>
-	<button id="btn_article_sort"><span class="fas fa-sort"></span> {{___('Sortowanie')}}</button>
+	<button id="btn_forum_new"><span class="fas fa-plus"></span> {{___('Nowe forum')}}</button>
+	<button id="btn_forum_new"><span class="fas fa-sort"></span> {{___('Sortowanie')}}</button>
 	<input type="text" id="txt_filter" name="filter" placeholder="Szukaj"><button id="btn_article_filter"><span class="fas fa-search"></span> {{___('Szukaj')}}</button>
 </div>
 <div id="zmcms_website_articles_control_panel_content">
 	@include('themes.'.Config('zmcms.frontend.theme_name').'.backend.zmcms_website_forums_list')
 </div>
 {{-- <pre>{{print_r($resultset, true)}}</pre> --}}
+@push('custom_js')
+	<script type="text/javascript">
+		$('#btn_forum_new').on('click', function(e){
+			e.preventDefault();e.stopPropagation();
+			var o = $(this).attr('id'); 
+			// alert(o);
+			location.href = "/"+backend_prefix+"/forums/create/frm";
+			return false;
+		});
+	</script>
+@endpush
+{{-- location.href = "/"+backend_prefix+"/forums/create/frm"; --}}
 @endsection
