9a03f3e3db296559a6e6123ad4fb8700edb7a4d2
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-29 12:37:54 +0200

    Update zmcms_website_forums_frm.blade.php

--- src/backend/views/zmcms_website_forums_frm.blade.php
+++ src/backend/views/zmcms_website_forums_frm.blade.php
@@ -10,29 +10,29 @@
     <form class="micro12" id="zmcms_website_forum_frm" method="post" enctype="multipart/form-data">
 		<label class="micro12">
 		    <span class="micro12 mini2">Nazwa</span>
-		    <input id="wforums_title_txt" class="micro12 mini10 required" type="text" name="name" value="{{$data['data']->name ?? null}}" placeholder="Tytuł">
+		    <input id="wforums_title_txt" class="micro12 mini10 required" type="text" name="name" value="{{$data[0]->name ?? null}}" placeholder="Tytuł">
 		    <span class="micro12 mini2">Slug</span>
-		    <input id="wforums_title_slug" class="micro12 mini10 required" type="text" name="slug" value="{{ $data['data']->slug ?? null }}" placeholder="Slug">
+		    <input id="wforums_title_slug" class="micro12 mini10 required" type="text" name="slug" value="{{ $data[0]->slug ?? null }}" placeholder="Slug">
 		</label>
 		<label class="micro12">
 		    <span class="micro12">Intro</span>
-		    <textarea class="richeditor micro12" type="text" cols="25" rows="40" name="intro" placeholder="Wstęp do właściwej treści artykułu" >
-		        {{$data['data']->intro ?? null}}
+		    <textarea class="richeditor micro12" id="wforums_intro" type="text" cols="25" rows="40" name="intro" placeholder="Wstęp do właściwej treści artykułu" >
+		        {!!$data[0]->intro!!}
 		    </textarea>
 		</label>
 		<label class="micro12">
-		<input id="wforums_token" class="micro12" type="text" name="token" value="{{$data['data']->slug ?? null}}" placeholder="Token">
+		<input id="wforums_token" class="micro12" type="text" name="token" value="{{$data[0]->slug ?? null}}" placeholder="Token">
 		<input id="wforums_action" class="micro12" type="text" name="action" value="{{ $settings['action'] }}" placeholder="Akcja">
-		<input id="wforums_access" class="micro12" type="text" name="access" value="{{ $data['data']->access ?? '*' }}" placeholder="Dostęp">
-		<input id="wforums_langs_id" class="micro12" type="text" name="langs_id" value="{{ $data['data']->langs_id ?? Session::get('language') }}" placeholder="Język">
+		<input id="wforums_access" class="micro12" type="text" name="access" value="{{ $data[0]->access ?? '*' }}" placeholder="Dostęp">
+		<input id="wforums_langs_id" class="micro12" type="text" name="langs_id" value="{{ $data[0]->langs_id ?? Session::get('language') }}" placeholder="Język">
 		
 	</label>
 		<button>{{ $settings['btnsave'] }}</button>
 	</form>
-	{{-- @if(isset($data)) --}}
-	{{-- {{Session::get('language')}} --}}
-	{{-- <pre>{{print_r($settings, true)}}</pre> --}}
-	{{-- @endif --}}
+	@if(isset($data))
+	{{Session::get('language')}}
+	<pre>{{print_r($data[0], true)}}</pre>
+	@endif
 @endsection
 @push('custom_js')
 <script type="text/javascript">
