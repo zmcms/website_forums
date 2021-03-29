6bdd3aca136cd9647e36fb788f864476edd7b254
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-29 09:18:24 +0200

    Update Queries.php

--- src/backend/db/Queries.php
+++ src/backend/db/Queries.php
@@ -7,21 +7,22 @@ class Queries{
 	public static function forum_create($data){
 		$forum = (Config('database.prefix')??'').'website_forums';
 		$forum_name = (Config('database.prefix')??'').'website_forums_names'; // Nazwy forum
+		$token = hash ('sha256', date('Ymd').rand(0,1000));
 		try{
 			DB::beginTransaction();
 				DB::table($forum)->insert([
-					'token'		=>	$data['data']['token'],
-					'access'	=>	$data['data']['access'],
-					'active'	=>	$data['data']['active'],
-					'obj_type'	=>	$data['data']['obj_type'],
-					'obj_token'	=>	$data['data']['obj_token'],
+					'token'		=>	$token,
+					'access'	=>	$data['access'],
+					'active'	=>	$data['active'] ?? 0,
+					'obj_type'	=>	$data['obj_type'] ?? null,
+					'obj_token'	=>	$data['obj_token'] ?? null,
 				]);
 				DB::table($forum_name)->insert([
-					'token'		=>	$data['data']['token'],
-					'langs_id'	=>	$data['data']['langs_id'],
-					'name'		=>	$data['data']['name'],
-					'slug'		=>	$data['data']['slug'],
-					'intro'		=>	$data['data']['intro'],
+					'token'		=>	$token,
+					'langs_id'	=>	$data['langs_id'],
+					'name'		=>	$data['name'],
+					'slug'		=>	$data['slug'],
+					'intro'		=>	$data['intro'],
 				]);
 			DB::commit();
 			return json_encode([
@@ -33,7 +34,7 @@ class Queries{
 			DB::rollBack();
 			return json_encode([
 				'result'	=>	'error',
-				'code'		=>	$e->getCode(),
+				'code'		=>	$e->getCode().$e->getMessage(),
 				'msg' 		=>	___('Nie można utworzyć nowego forum'),
 			]);
 		}
@@ -100,14 +101,14 @@ class Queries{
 		$forum_name = (Config('database.prefix')??'').'website_forums_names'; // Nazwy forum
 		try{
 			DB::beginTransaction();
-				DB::table($forum)where('token', $token)->update([
+				DB::table($forum)->where('token', $token)->update([
 					'token'		=>	$data['data']['token'],
 					'access'	=>	$data['data']['access'],
 					'active'	=>	$data['data']['active'],
 					'obj_type'	=>	$data['data']['obj_type'],
 					'obj_token'	=>	$data['data']['obj_token'],
 				]);
-				DB::table($forum_name)where('token', $token)->update([
+				DB::table($forum_name)->where('token', $token)->update([
 					'token'		=>	$data['data']['token'],
 					'langs_id'	=>	$data['data']['langs_id'],
 					'name'		=>	$data['data']['name'],
