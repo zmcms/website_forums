4f737729db0f10d87c951b1c71460e0191bfce48
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-29 23:53:23 +0200

    Add files via upload

--- src/frontend/controllers/ZmcmsWebsiteForumsController.php
+++ src/frontend/controllers/ZmcmsWebsiteForumsController.php
@@ -3,5 +3,7 @@ namespace Zmcms\WebsiteForums\Frontend\Controllers;
 use Illuminate\Http\Request;
 class ZmcmsWebsiteForumsController extends \App\Http\Controllers\Controller
 {
-
+	public static function connect_forum(){
+		return 'forum connected';
+	}
 }
