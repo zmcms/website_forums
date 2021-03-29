69ec4b3ef223a8d48fffb072b5250c82d5ec6011
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-29 09:20:07 +0200

    Update ZmcmsWebsiteForumsController.php

--- src/backend/controllers/ZmcmsWebsiteForumsController.php
+++ src/backend/controllers/ZmcmsWebsiteForumsController.php
@@ -1,14 +1,56 @@
 <?php
 namespace Zmcms\WebsiteForums\Backend\Controllers;
 use Illuminate\Http\Request;
+use Zmcms\WebsiteForums\Backend\Db\Queries as Q;
 class ZmcmsWebsiteForumsController extends \App\Http\Controllers\Controller
 {
-	public function forums_list(){}
-	public function forums_save(){}
-	public function zmcms_website_forums_filter(){}
-	public function forums_edit(){}
-	public function zmcms_website_forums_paginator(){}
-	public function zmcms_website_forums_page_delete(){}
-	public function forums_create_frm(){}
-	public function zmcms_website_article_delete(){}
+	public function forums_list(Request $request){
+		$data = [];
+		$settings=[
+			'title'	=> ___('Lista forów'),
+			'action' => 'create',
+			'btnsave' => ___('Zapisz'),
+			'btnsave_and_publish' => __('Zapisz i opublikuj'),
+		];
+		return view('themes.'.(Config('zmcms.frontend.theme_name') ?? 'zmcms').'.backend.zmcms_website_forums_list', compact('data', 'settings'));
+	}
+	public function forums_create_frm(Request $request){
+		$data = $request->all();
+		$settings=[
+			'title'	=> ___('Nowe forum'),
+			'action' => 'create',
+			'btnsave' => ___('Zapisz'),
+			'btnsave_and_publish' => __('Zapisz i opublikuj'),
+		];
+		
+		return view('themes.'.(Config('zmcms.frontend.theme_name') ?? 'zmcms').'.backend.zmcms_website_forums_frm', compact('data', 'settings'));
+	}
+	public function forums_save(Request $request){
+		$data = $request->all();
+		// return '<pre>'.print_r($data, true).'</pre>'; 	
+		// GDY AKCJA NIE USTAWIONA
+		if(!isset($data['action']))
+			return json_encode([
+				'result'	=>	'error',
+				'code'		=>	$e->getCode(),
+				'msg' 		=>	___('Nie można utworzyć nowego forum'),
+			]);
+		switch ($data['action']) {
+			case 'create':	{ return Q::forum_create($data); break; }
+			case 'update':	{break;}
+			case 'delete':	{break;}
+			default:		{break;}
+		}
+
+		
+	}
+	public function forums_update_frm(Request $request){
+	
+	}
+	public function forums_configuration_frm(Request $request){
+	
+	}
+	public function forums_create_frm_save(Request $request){
+
+	}
 }
