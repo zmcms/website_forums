df003f60bf8edc1f08fad406da0e30868c76d3c4
zmcms <59611848+zmcms@users.noreply.github.com>
2021-03-29 09:22:40 +0200

    Update website_forums.php

--- src/backend/routes/website_forums.php
+++ src/backend/routes/website_forums.php
@@ -1,27 +1,34 @@
 <?php
 Route::middleware(['BackendUser'])->group(function () {
+	$prefix = Config('zmcms.main.backend_prefix');
 	/**
 	 * WYŚWIETLA LISTA FORÓW W SYSTEMIE
 	 */
-	Route::get($prefix.'/forums/list', 
-		'Zmcms\WebsiteArticles\Backend\Controllers\ZmcmsWebsiteForumsController@forums_list')
+	Route::any($prefix.'/forums/list', 
+		'Zmcms\WebsiteForums\Backend\Controllers\ZmcmsWebsiteForumsController@forums_list')
 		->name('website_forums');
 	/**
 	 * WYŚWIETLA FORMULARZ W TRYBIE DODAWANIA NOWEGO FORUM 
 	 */
-	Route::get($prefix.'/forums/create/frm',
-		'Zmcms\WebsiteArticles\Backend\Controllers\ZmcmsWebsiteForumsController@forums_create_frm')	
+	Route::any($prefix.'/forums/create/frm',
+		'Zmcms\WebsiteForums\Backend\Controllers\ZmcmsWebsiteForumsController@forums_create_frm')	
 		->name('website_forums');
 	/**
 	 * WYŚWIETLA FORMULARZ W TRYBIE AKTUALIZACJI FORUM 
 	 */
-	Route::get($prefix.'/forums/update/frm/{token}',
-		'Zmcms\WebsiteArticles\Backend\Controllers\ZmcmsWebsiteForumsController@forums_create_frm')	
+	Route::any($prefix.'/forums/update/frm/{token}',
+		'Zmcms\WebsiteForums\Backend\Controllers\ZmcmsWebsiteForumsController@forums_update_frm')	
+		->name('website_forums');
+	/**
+	 * Zapisywanie zmian w formularzu 
+	 */
+	Route::post($prefix.'/forums/create/save',
+		'Zmcms\WebsiteForums\Backend\Controllers\ZmcmsWebsiteForumsController@forums_save')
 		->name('website_forums');
 	/**
 	 * Konfiguracja forum (plik w katalogu /config/motyw/website_forums.php)
 	 */
-	Route::get($prefix.'/forums/configuration',
-		'Zmcms\WebsiteArticles\Backend\Controllers\ZmcmsWebsiteForumsController@forums_create_frm')	
+	Route::any($prefix.'/forums/configuration',
+		'Zmcms\WebsiteForums\Backend\Controllers\ZmcmsWebsiteForumsController@forums_configuration_frm')	
 		->name('website_forums');
 });
