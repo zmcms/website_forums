07bac5eaad42eb317a50cf772a55b0477448b004
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-22 23:23:50 +0200

    Update zmcms_website_forums_frm.blade.php

--- src/backend/views/zmcms_website_forums_frm.blade.php
+++ src/backend/views/zmcms_website_forums_frm.blade.php
@@ -1,33 +1,29 @@
-@extends('themes.zmcms.backend.main')
+@extends('themes.'.Config('zmcms.frontend.theme_name').'.backend.main')
 @section('content')
 <h1 class="">{{$settings['title'] ?? ___('Forum')}}</h1>
-<div class="control_belt">
-    <div class="micro12 ">
-        <button class="back_btn"><span class="fas fa-angle-left"></span>Powrót</button>
-    </div>
-</div>
 <div>
     <form class="micro12" id="zmcms_website_forum_frm" method="post" enctype="multipart/form-data">
-		<label class="micro12">
-		    <span class="micro12 mini2">Nazwa</span>
-		    <input id="wforums_title_txt" class="micro12 mini10 required" type="text" name="name" value="{{$data[0]->name ?? null}}" placeholder="Tytuł">
-		    <span class="micro12 mini2">Slug</span>
-		    <input id="wforums_title_slug" class="micro12 mini10 required" type="text" name="slug" value="{{ $data[0]->slug ?? null }}" placeholder="Slug">
-		</label>
-		<label class="micro12">
-		    <span class="micro12">Intro</span>
-		    <textarea class="richeditor micro12" id="wforums_intro" type="text" cols="25" rows="40" name="intro" placeholder="Wstęp do właściwej treści artykułu" >
+		<div class="micro12">
+		    <input id="wforums_title_txt" type="text" name="name" value="{{$data[0]->name ?? null}}" placeholder=" ">
+		    <label>Nazwa</label>
+		</div>
+		<div class="micro12">
+			<input id="wforums_title_slug" type="text" name="slug" value="{{ $data[0]->slug ?? null }}" placeholder=" ">
+			<label>Slug</label>
+		</div>
+		<div class="micro12">
+		    <h3>Intro</h3>
+		    <textarea class="richeditor" id="wforums_intro" type="text" cols="25" rows="40" name="intro" placeholder=" " >
 		        {!! $data[0]->intro ?? null!!}
 		    </textarea>
-		</label>
-		<label class="micro12">
-		<input id="wforums_token" class="micro12" type="text" name="token" value="{{$data[0]->token ?? null}}" placeholder="Token">
-		<input id="wforums_action" class="micro12" type="text" name="action" value="{{ $settings['action'] }}" placeholder="Akcja">
-		<input id="wforums_access" class="micro12" type="text" name="access" value="{{ $data[0]->access ?? '*' }}" placeholder="Dostęp">
-		<input id="wforums_langs_id" class="micro12" type="text" name="langs_id" value="{{ $data[0]->langs_id ?? Session::get('language') }}" placeholder="Język">
-		
-	</label>
-		<button>{{ $settings['btnsave'] }}</button>
+		</div>
+		<input id="wforums_token" class="micro12" type="hidden" name="token" value="{{$data[0]->token ?? null}}" placeholder="Token">
+		<input id="wforums_action" class="micro12" type="hidden" name="action" value="{{ $settings['action'] }}" placeholder="Akcja">
+		<input id="wforums_access" class="micro12" type="hidden" name="access" value="{{ $data[0]->access ?? '*' }}" placeholder="Dostęp">
+		<input id="wforums_langs_id" class="micro12" type="hidden" name="langs_id" value="{{ $data[0]->langs_id ?? Session::get('language') }}" placeholder="Język">
+		{{-- <div class="micro12"> --}}
+			<button>{{ $settings['btnsave'] }}</button>
+		{{-- </div> --}}
 	</form>
 	{{-- @if(isset($data)) --}}
 	{{-- {{Session::get('language')}} --}}
@@ -36,6 +32,7 @@
 @endsection
 @push('custom_js')
 <script type="text/javascript">
+	var backend_prefix = $('meta[name="backend-prefix"]').attr('content');
 	$('#wforums_title_txt').on('keyup', function(e){
 		$('#wforums_title_slug').val(str_slug($('#wforums_title_txt').val()));
 	});
@@ -63,15 +60,3 @@
 	})
 </script>
 @endpush
-
-{{-- 
-
-token
-access
-active
-obj_type
-obj_token
-langs_id
-name
-slug
-intro --}}
